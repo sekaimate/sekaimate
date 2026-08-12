62a9e926ce44635a84466ce399aaa049ac6b8643
AkaakuHub <akaaku2021media01@gmail.com>
2026-08-13 02:15:33 +0900

    fix: populate web storage before startup

--- Basis/Packages/com.basis.framework/Device Management/Boot Sequence/BasisBootSequence.cs
+++ Basis/Packages/com.basis.framework/Device Management/Boot Sequence/BasisBootSequence.cs
@@ -68,6 +68,10 @@ namespace Basis.Scripts.Boot_Sequence
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
         static async Task OnAfterSceneLoadRuntimeMethod()
         {
+#if UNITY_WEBGL && !UNITY_EDITOR
+            await BasisWebPersistence.EnsureInitializedAsync();
+#endif
+
             // Subscribe once to teardown hooks.
             if (!HasEvents)
             {
