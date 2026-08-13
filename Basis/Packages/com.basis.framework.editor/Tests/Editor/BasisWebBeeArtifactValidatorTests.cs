77d32d91b75c567015d49a070d59c964a4634262
AkaakuHub <akaaku2021media01@gmail.com>
2026-08-13 10:16:38 +0900

    test: define WebGL avatar BEE artifact

--- Basis/Packages/com.basis.framework.editor/Tests/Editor/BasisWebBeeArtifactValidatorTests.cs
+++ Basis/Packages/com.basis.framework.editor/Tests/Editor/BasisWebBeeArtifactValidatorTests.cs
@@ -59,7 +59,28 @@ public class BasisWebBeeArtifactValidatorTests
         Assert.That(valid, Is.True, error);
     }
 
-    private static BasisBundleConnector CreateConnector()
+    [Test]
+    public void AcceptsSingleWebGlAvatarSectionWithExactFileLength()
+    {
+        BasisBundleConnector connector = CreateConnector("GameObject", "BasisAvatar");
+
+        bool valid = BasisWebBeeArtifactValidator.TryValidateAvatar(connector, 64, 200, out string error);
+
+        Assert.That(valid, Is.True, error);
+    }
+
+    [TestCase("Scene", "BasisAvatar")]
+    [TestCase("GameObject", "BasisProp")]
+    public void RejectsAvatarArtifactWithWrongModeOrContentType(string assetMode, string componentName)
+    {
+        BasisBundleConnector connector = CreateConnector(assetMode, componentName);
+
+        bool valid = BasisWebBeeArtifactValidator.TryValidateAvatar(connector, 64, 200, out _);
+
+        Assert.That(valid, Is.False);
+    }
+
+    private static BasisBundleConnector CreateConnector(string assetMode = "Scene", string componentName = null)
     {
         return new BasisBundleConnector
         {
@@ -67,13 +88,26 @@ public class BasisWebBeeArtifactValidatorTests
             {
                 new BasisBundleGenerated(
                     "hash",
-                    "Scene",
-                    "scene-name",
+                    assetMode,
+                    "asset-name",
                     42,
                     true,
                     "password",
                     "WebGL",
                     128),
+            },
+            MetaData = new BasisBundleConnector.BasisMetaData
+            {
+                ComponentNames = componentName == null
+                    ? new BasisBundleConnector.BasisComponentName[0]
+                    : new[]
+                    {
+                        new BasisBundleConnector.BasisComponentName
+                        {
+                            Name = componentName,
+                            count = 1,
+                        },
+                    },
             },
         };
     }
