53ae519c01f0294be91e057f91b8c411f863c10d
AkaakuHub <akaaku2021media01@gmail.com>
2026-08-13 10:59:05 +0900

    test: define individual player action permissions

---
+++ Basis/Packages/com.basis.framework.editor/Tests/Editor/BasisIndividualPlayerActionPermissionsTests.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using Basis.BasisUI;
+using BasisNetworkCore.Security;
+using NUnit.Framework;
+
+public sealed class BasisIndividualPlayerActionPermissionsTests
+{
+    [TestCase(PermNodes.ModerationKick, IndividualPlayerAdminAction.Kick)]
+    [TestCase(PermNodes.ModerationBan, IndividualPlayerAdminAction.Ban)]
+    [TestCase(PermNodes.ModerationIpBan, IndividualPlayerAdminAction.IpBan)]
+    [TestCase(PermNodes.ModerationTeleport, IndividualPlayerAdminAction.Teleport)]
+    [TestCase(PermNodes.ModerationShout, IndividualPlayerAdminAction.Shout)]
+    [TestCase(PermNodes.ModerationMessage, IndividualPlayerAdminAction.Message)]
+    [TestCase(PermNodes.PermissionsEdit, IndividualPlayerAdminAction.EditPermissions)]
+    public void AllowsOnlyActionMatchingEffectivePermission(
+        string permission,
+        IndividualPlayerAdminAction expectedAction)
+    {
+        HashSet<string> permissions = new() { permission };
+
+        foreach (IndividualPlayerAdminAction action in System.Enum.GetValues(typeof(IndividualPlayerAdminAction)))
+        {
+            Assert.AreEqual(
+                action == expectedAction,
+                IndividualPlayerActionPermissions.CanUse(permissions, action),
+                action.ToString());
+        }
+    }
+
+    [Test]
+    public void PermissionsViewDoesNotAuthorizeMutatingActions()
+    {
+        HashSet<string> permissions = new() { PermNodes.PermissionsView };
+
+        foreach (IndividualPlayerAdminAction action in System.Enum.GetValues(typeof(IndividualPlayerAdminAction)))
+        {
+            Assert.IsFalse(IndividualPlayerActionPermissions.CanUse(permissions, action), action.ToString());
+        }
+    }
+
+    [Test]
+    public void AnySpecificActionOrPermissionsViewMakesAdminSectionVisible()
+    {
+        Assert.IsTrue(IndividualPlayerActionPermissions.CanViewSection(
+            new HashSet<string> { PermNodes.PermissionsView }));
+        Assert.IsTrue(IndividualPlayerActionPermissions.CanViewSection(
+            new HashSet<string> { PermNodes.ModerationKick }));
+        Assert.IsFalse(IndividualPlayerActionPermissions.CanViewSection(new HashSet<string>()));
+    }
+}
