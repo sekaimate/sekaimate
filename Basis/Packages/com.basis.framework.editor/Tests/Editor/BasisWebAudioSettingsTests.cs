444fe73615d303c7568260bf48c1cc8edfa57d25
AkaakuHub <akaaku2021media01@gmail.com>
2026-08-13 14:03:04 +0900

    fix: preload microphone toggle sounds on WebGL

--- Basis/Packages/com.basis.framework.editor/Tests/Editor/BasisWebAudioSettingsTests.cs
+++ Basis/Packages/com.basis.framework.editor/Tests/Editor/BasisWebAudioSettingsTests.cs
@@ -1,8 +1,16 @@
 using System.IO;
 using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
 
 public class BasisWebAudioSettingsTests
 {
+    private static readonly string[] MicrophoneToggleSounds =
+    {
+        "Packages/com.basis.sdk/Sounds/boop on.wav",
+        "Packages/com.basis.sdk/Sounds/boop off.wav",
+    };
+
     [Test]
     public void SteamAudioDoesNotEnableWebGlRuntime()
     {
@@ -11,5 +19,19 @@ public class BasisWebAudioSettingsTests
 
         StringAssert.DoesNotContain("NamedBuildTarget.iOS,\n                NamedBuildTarget.WebGL", source);
         StringAssert.Contains("webDefineList.Remove(\"STEAMAUDIO_ENABLED\")", source);
+    }
+
+    [Test]
+    public void MicrophoneToggleSoundsArePreloadedForWebGl()
+    {
+        foreach (string path in MicrophoneToggleSounds)
+        {
+            AudioImporter importer = AssetImporter.GetAtPath(path) as AudioImporter;
+
+            Assert.That(importer, Is.Not.Null, path);
+            AudioImporterSampleSettings settings = importer.defaultSampleSettings;
+            Assert.That(settings.loadType, Is.EqualTo(AudioClipLoadType.DecompressOnLoad), path);
+            Assert.That(settings.preloadAudioData, Is.True, path);
+        }
     }
 }
