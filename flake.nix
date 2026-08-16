da5a2a5de773231856aa3fbb368c6ac40a30f544
mimifuwacc <mail@mimifuwa.cc>
2026-08-16 19:27:52 +0900

    chore: add flake.nix and envrc to install pnpm

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "sekaimate/sekaimate";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs_24
+        pnpm
+      ];
+    };
+  });
+}
